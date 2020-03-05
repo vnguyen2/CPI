1662af61383f6a8c89e4f4398385e5f24d15c3fa
Vincent Nguyen <vnguyen@vincents-mbp.lan>
2020-03-05 17:00:02 -0500

    dev environment functional

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,76 @@
 
 // Add our dependencies
-const gulp = require('gulp'), // Main Gulp module
-    browsersync = require("browser-sync").create();
-    concat = require('gulp-concat'), // Gulp File concatenation plugin
-    open = require('gulp-open'), // Gulp browser opening plugin
-    connect = require('gulp-connect'); // Gulp Web server runner plugin
+const gulp = require('gulp'); // Main Gulp module
+const concat = require('gulp-concat'); // Gulp File concatenation plugin   
+const connect = require('gulp-connect'); // Gulp Web server runner plugin
+const open = require('gulp-open');
 
-// BrowserSync
-function browserSync(done) {
-    browsersync.init({
-      server: {
-        baseDir: "./app/"
-      },
-      port: 3000
-    });
-    done();
-  }
-  
-  // BrowserSync Reload
-  function browserSyncReload(done) {
-    browsersync.reload();
-    done();
-  }
+const configuration = {
+        paths: {
+            app: {
+                html: './app/*.html',
+                css: [
+                    './app/css/main.css'
+                ]
+            },
+            dist: './dist'
+        },
+        localServer: {
+            port: 3000,
+            url: 'http://localhost:3000/'
+        }
+    };
+    
 
 // Gulp task to copy HTML files to output directory
 function html(){
-    gulp.src(configuration.paths.app.html)
+    return gulp
+        .src(configuration.paths.app.html)
         .pipe(gulp.dest(configuration.paths.dist))
         .pipe(connect.reload());
+        
 }
 
 // Gulp task to concatenate our css files
 function css(){
-    gulp.src(configuration.paths.app.css)
+    return gulp
+        .src(configuration.paths.app.css)
         .pipe(concat('site.css'))
         .pipe(gulp.dest(configuration.paths.dist + '/css'))
         .pipe(connect.reload());
 }
 
 // Gulp task to create a web server
-function connect(){
+function serverTask(done){
     connect.server({
-        root: 'dist',
-        port: 3000,
+        root: './dist',
+        port: configuration.localServer.port,
         livereload: true
     })
+    done();
+}
+
+//Gulp task to open default web browser
+function openPage(){
+    return gulp
+        .src('dist/index.html')
+        .pipe(open({uri: configuration.localServer.url}));
 }
 
 // Watch the file system and reload the website automatically
-function watch(){
-    gulp.watch(configuration.paths.app.html, ['html']);
-    gulp.watch(configuration.paths.app.css, ['css']);
-    gulp.series(browserSyncReload);
+function watchFiles(done){
+    gulp.watch(configuration.paths.app.html, html);
+    gulp.watch(configuration.paths.app.css, css);
+    done()
 }
 
 // Gulp
-const build = gulp.parallel(html, css);
-const watch = gulp.parallel(watch, browserSync);
+const build = gulp.series(html, css, serverTask, openPage, watchFiles);
+
+exports.html = html;
+exports.css = css;
+exports.serverTask = serverTask;
+exports.openPage = openPage;
+exports.watchFiles = watchFiles;
+exports.build = build;
+exports.default = build;
